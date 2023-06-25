654268b1162a74bff8e58431b32184b13ecd0fb5
weizhou <weizhou_hb@163.com>
2023-06-25 19:31:42 +0800

    add jerry imx6ull emmc board

---
+++ imx6ull_build_shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_ddr256_nand_defconfig 
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_jerry_emmc_defconfig
+make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j12
+# mx6ull_14x14_evk_emmc_defconfig  sudo apt-get install libssl-dev bison
